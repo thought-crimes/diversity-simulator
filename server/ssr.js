ad8b1764cb1f855deb42b012a7d9edb1b4024453
jstrop <jstrop@techsera.com>
2018-09-30 22:36:37 -0400

    cleaning up

--- server/ssr.js
+++ server/ssr.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { Provider } from 'react-redux';
-import axios from 'axios';
 
 import App from '../src/components/App';
 import storeConfig from '../src/store/storeConfig';
 
 module.exports = async () => {
-  const resp = await axios.get('https://bakesaleforgood.com/api/deals');
 
-  const initialData = {
-    deals: resp.data.slice(0, 4),
-  };
+  const initialData = {};
 
   const store = storeConfig(initialData);
 
--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import DealList from './DealList';
-import FetchMoreDealsButton from './FetchMoreDealsButton';
-import Clock from './Clock';
-import { doSearch } from '../store/actions';
-import ErrorDisplay from './ErrorDisplay';
-
 class App extends React.Component {
   state = {
-    inputValue: '',
-  };
-  handleSearchSubmit = (event) => {
-    event.preventDefault();
-
-    this.props.doSearch(this.state.inputValue);
-  };
-  handleInputChange = (event) => {
-    const newValue = event.target.value;
-    this.setState(() => ({ inputValue: newValue }));
+    inputValue: ''
   };
   render() {
     return (
       <React.Fragment>
-        <Clock />
-        <div className="app">
-          <form onSubmit={this.handleSearchSubmit}>
-            <input
-              type="search"
-              placeholder="Search all deals"
-              value={this.state.inputValue}
-              onChange={this.handleInputChange}
-            />
-            <button type="submit">Search</button>
-          </form>
-          <DealList />
-          <ErrorDisplay />
-          <FetchMoreDealsButton />
-        </div>
+        <div className="app" />
       </React.Fragment>
     );
   }
 }
 
-export default connect(
-  null,
-  { doSearch }
-)(App);
+export default connect()(App);
